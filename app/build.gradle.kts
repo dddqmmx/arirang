9596128a3fb3a179e72faba498c366e9138e922d
dddqmmx <dddqmmx@gmail.com>
2026-04-25 01:06:55 +0800

    chore: prepare for 0.1.0-alpha release and fix signing path

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 34
         targetSdk = 36
         versionCode = 1
-        versionName = "1.0"
+        versionName = "0.1.0-alpha"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         multiDexEnabled = false
         resConfigs("en", "zh-rCN", "ja")
